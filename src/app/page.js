e2120a0f0a18fe898d1bb1db76dc8ccffdbdfd8a
Jay Godara <jaygodara10062006@gmail.com>
2024-10-10 20:22:24 +0530

    second

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar"
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <h2 className="text-3xl font-bold text-center p-5">Yokoso! Watashi no Website</h2>
+      <h3 className="text-2xl font-bold text-center p-5">godara bhai aage ka dekh lena</h3>
+    </>
+  )
+}
+
+export default Home
